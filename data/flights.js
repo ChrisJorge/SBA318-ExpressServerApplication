4984355d6781a48f1bd9e87fd2164646814ab84d
Chris <Christopherjorge47@icloud.com>
2024-04-20 16:36:02 -0700

    Added flights data and imported it to server

---
+++ data/flights.js
@@ -0,0 +1,39 @@
+const flights = [
+    {
+        id: 1,
+        departing: 'SFO',
+        arriving: 'EWR',
+        time: '5:45',
+        cost: '197'
+    },
+    {
+        id: 2,
+        departing: 'SFO',
+        arriving: 'LAX',
+        time: '1:25',
+        cost: '91'
+    },
+    {
+        id: 3,
+        departing: 'SFO',
+        arriving: 'JFK',
+        time: '5:33',
+        cost: '297'
+    },
+    {
+        id: 4,
+        departing: 'HKG',
+        arriving: 'SFO',
+        time: '17:10',
+        cost: '617'
+    },
+    {
+        id: 5,
+        departing: 'EWR',
+        arriving: 'BCN',
+        time: '10:55',
+        cost: '508'
+    },
+]
+
+module.exports = flights
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+const flights = require('./data/flights.js')
+
 app.get('/', (req, res) => {
     res.send('Welcome to port 3000!!')
 })
