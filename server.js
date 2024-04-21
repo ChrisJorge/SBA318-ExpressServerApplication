10f7a8bc2ccce85809ec0e91dea7b40c29703109
Chris <Christopherjorge47@icloud.com>
2024-04-20 17:08:54 -0700

    added paths to display all the data

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const port = process.env.PORT || 3000;
 const flights = require('./data/flights.js')
 const airports = require('./data/airports.js')
 const planes = require('./data/planes.js')
-console.log(planes)
+
 app.get('/', (req, res) => {
-    res.send('Welcome to port 3000!!')
+    res.send('Welcome Please go to /flights to see the available flights, /airports for airports, and /planes for planes!')
 })
 
+app.get('/flights', (req,res) => {
+    res.send(flights)
+})
+app.get('/planes', (req,res) => {
+    res.send(planes)
+})
+app.get('/airports', (req,res) => {
+    res.send(airports)
+})
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
