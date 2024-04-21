8c3f4b2a3f7425346585ed750b0c1ed9dbe8a626
Chris <Christopherjorge47@icloud.com>
2024-04-21 15:19:53 -0400

    patch request working for flights.js

--- routes/flights.js
+++ routes/flights.js
@@ -14,22 +14,26 @@ router
        
         .post((req, res, next) => {
             console.log('sending data')
-            if (req.body.id && req.body.departing && req.body.arriving && req.body.time && req.body.cost) {
-            if (flights.find((flight) => flight.id == req.body.id)) {
-                next(error(409, "id Already Taken"));
+            if (req.body.id && req.body.departing && req.body.arriving && req.body.time && req.body.cost)
+            {
+                if (flights.find((flight) => flight.id == req.body.id)) {
+                    next(error(409, "id Already Taken"));
+                }
+            
+                const flight = {
+                    id: req.body.id,
+                    departing: req.body.departing,
+                    arriving: req.body.arriving,
+                    time: req.body.time,
+                    cost: req.body.cost
+                };
+            
+                flights.push(flight);
+                res.json(flights[flights.length - 1]);
             }
-        
-            const flight = {
-                id: req.body.id,
-                departing: req.body.departing,
-                arriving: req.body.arriving,
-                time: req.body.time,
-                cost: req.body.cost
-            };
-        
-            flights.push(flight);
-            res.json(flights[flights.length - 1]);
-             }
+            else{
+                next(error(400,"Not Enough Information"))
+            }
         });
 
         router
@@ -38,10 +42,31 @@ router
                     const flight = flights.find((flight) => flight.id == req.params.id);
                     if (flight) 
                     {
-                        // res.json(flight);
+                       res.json(flight);
                     }
                         else next();
                 })
+
+            .patch((req,res,next) => {
+                const flight = flights.find((flight, i) => {
+                    if(flight.id == req.params.id)
+                    {
+                        for(const key in req.body)
+                        {
+                            flights[i][key] = req.body[key]
+                        }
+                    }
+                    return true
+                })
+                if(flight)
+                {
+                    res.json(flight);
+                }
+                else
+                {
+                    next()
+                }
+            })
                 
 
 module.exports = router;
